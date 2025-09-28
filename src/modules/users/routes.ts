adcb99aa9165f5036d65d9ac08e4d1d865470bca
contactochiga <contactochiga@gmail.com>
2025-09-28 19:04:08 +0100

    Create routes.ts

---
+++ src/modules/users/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { getMe, updateMe } from "./user.controller";
+import { requireAuth } from "../../middleware/auth";
+
+const router = Router();
+
+router.get("/me", requireAuth, getMe);
+router.put("/me", requireAuth, updateMe);
+
+export default router;
