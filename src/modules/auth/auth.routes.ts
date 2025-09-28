0bc3531a724f3b434778f01333d3568e1a080549
contactochiga <contactochiga@gmail.com>
2025-09-28 19:20:11 +0100

    Update auth.routes.ts

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,11 +1,14 @@
+// src/modules/auth/auth.routes.ts
 import { Router } from "express";
 import { register, login, refreshToken, logout } from "./auth.controller";
+import { validate } from "../../middleware/validate";
+import { RegisterSchema, LoginSchema, RefreshSchema } from "./auth.validation";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
-router.post("/refresh", refreshToken);
+router.post("/register", validate(RegisterSchema), register);
+router.post("/login", validate(LoginSchema), login);
+router.post("/refresh", validate(RefreshSchema), refreshToken);
 router.post("/logout", logout);
 
 export default router;
